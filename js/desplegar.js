5c612e20e417045bc0caa337025bc1339f51ec6e
RosbelDesing <108763802+RosbelDesing@users.noreply.github.com>
2022-07-07 19:56:20 -0400

    Add files via upload

---
+++ js/desplegar.js
@@ -0,0 +1,5 @@
+const btn=document.querySelector(".btn")
+const search=document.querySelector(".search-box1")
+
+btn.addEventListener("click",() => {
+    search.classList.toggle("search-box1-visible");})
