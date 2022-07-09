6e98de23af025e74ab3ee5d257858cab5c9c0b48
RosbelDesing <108763802+RosbelDesing@users.noreply.github.com>
2022-07-09 18:36:08 -0400

    Add files via upload

--- js/menu2.js
+++ js/menu2.js
@@ -3,3 +3,12 @@ const navMenu=document.querySelector(".nav-menu")
 
 navToggle.addEventListener("click",() => {
     navMenu.classList.toggle("nav-menu-circulo");})
+
+
+const menuLinks = document.querySelectorAll(".nav-menu a[href^=\"#\"]");
+
+menuLinks.forEach(menuLink =>{ 
+    menuLink.addEventListener("click",function() {
+        navMenu.classList.remove("nav-menu-circulo");
+    })
+});
